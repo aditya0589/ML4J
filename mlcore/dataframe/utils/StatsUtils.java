89b7d1f5f189fab70825437844cc49966950f0ff
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2025-09-28 21:19:46 +0530

    Update StatsUtils.java

--- mlcore/dataframe/utils/StatsUtils.java
+++ mlcore/dataframe/utils/StatsUtils.java
@@ -2,7 +2,8 @@ package mlcore.dataframe.utils;
 
 public class StatsUtils {
     // Contains all the utility methods required for the calculation of other packages
-    // This includes the contents of mlcore.dataframe.transformations
+    // This supports the contents of mlcore.dataframe.transformations
     // includes statistical functions of mean, median, mode, variance, standard deviation
     
 }
+
