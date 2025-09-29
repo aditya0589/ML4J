a07875fd124cf210a9399966ce493de6e7e10479
Prithvi <mprithvi2005@gmail.com>
2025-09-29 20:07:44 +0530

    Update Encoder.java

--- mlcore/dataframe/transformations/Encoder.java
+++ mlcore/dataframe/transformations/Encoder.java
@@ -1,12 +1,59 @@
 package mlcore.dataframe.transformations;
 
-public class Encoder {
-    // Handles all kinds of feature encoding including but not limited to:
-     /* 1. ordinal encoding
-     * 2. one hot encoding
-     * 3. label encoding
-     */
-    // The input passed to methods of this class must be a Dataframe (mlcore.dataframe.DataFrame)
-    // The output returned from methods of this class must be a DataFrame (mlcore.dataframe.DataFrame)
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import mlcore.dataframe.DataFrame;
+
+public class Encoder {
+    public DataFrame labelEncoding(DataFrame df, String columnName) {
+        Map<String, List<Object>> labelEncoded = new LinkedHashMap<>(df.getData());
+
+        if (!labelEncoded.containsKey(columnName)) {
+            throw new IllegalArgumentException("Column " + columnName + " not found");
+        }
+
+        Map<Object, Integer> uniqMap = new HashMap<>();
+        int code = 0;
+
+        List<Object> encodedColumn = new ArrayList<>();
+
+        for (Object val : labelEncoded.get(columnName)) {
+            uniqMap.putIfAbsent(val, code++);
+            encodedColumn.add(uniqMap.get(val));
+        }
+
+        labelEncoded.remove(columnName);
+        labelEncoded.put(columnName + "_Encoded", encodedColumn);
+
+        return new DataFrame(labelEncoded);
+    }
+
+    public DataFrame labelEncoding(DataFrame df, List<String> columns) {
+        Map<String, List<Object>> labelEncoded = new LinkedHashMap<>(df.getData());
+
+        for (String col : columns) {
+            if (!labelEncoded.containsKey(col)) {
+                throw new IllegalArgumentException("Column " + col + " not found");
+            }
+
+            Map<Object, Integer> uniqMap = new HashMap<>();
+            int code = 0;
+            List<Object> encodedColumn = new ArrayList<>();
+
+            for (Object val : labelEncoded.get(col)) {
+                uniqMap.putIfAbsent(val, code++);
+                encodedColumn.add(uniqMap.get(val));
+            }
+
+            labelEncoded.remove(col);
+            labelEncoded.put(col + "_Encoded", encodedColumn);
+        }
+
+        return new DataFrame(labelEncoded);
+    }
+    
 }
