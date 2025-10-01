d59731596d5ad19933ea9dd376b7eed4bdbdd1f8
Prithvi <mprithvi2005@gmail.com>
2025-10-01 20:53:00 +0530

    Update OutlierHandler.java

--- mlcore/dataframe/transformations/OutlierHandler.java
+++ mlcore/dataframe/transformations/OutlierHandler.java
@@ -1,17 +1,15 @@
 package mlcore.dataframe.transformations;
 
-public class OutlierHandler {
-    // This class contains methods to handle outliers in the dataset
-    // This is done using the IQR method
-    
-    /* * * * * * * * * * * * * * * * * * * * * * * 
-     * IQR -> Interquartile Range                *
-     * calculated by the formula:                *
-     * IQR = Q3 - Q1                             *
-     * where Q3 = third quartile (75 percentile) *
-     * where q1 = first quartile (25 percentile) *
-     *  * * * * * * * * * * * * * * * * * * * *  */
+import mlcore.dataframe.DataFrame;
+import mlcore.dataframe.utils.StatsUtils;
 
-    // The input of the methods of this class must be of type DataFrame (mlcore.dataframe.DataFrame)
-    // The output of the methods of this class must be of type 
+public class OutlierHandler {
+
+    public Double IQR(DataFrame df, String columnName, StatsUtils stat) {
+        Double Q1 = stat.Q1Value(df, columnName);
+        Double Q3 = stat.Q3Value(df, columnName);
+        Double iqr = Q3 - Q1;
+        return iqr;
+    }
+    
 }
